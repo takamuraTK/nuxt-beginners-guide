91aa884c86c212d03dc82b6d491e01f6b35b967d
takamuraTK <tirpitz.zzz@outlook.jp>
2020-08-27 00:40:12 +0900

    Add title settings

--- my-first-nuxt-app/nuxt.config.js
+++ my-first-nuxt-app/nuxt.config.js
@@ -16,6 +16,7 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    titleTemplate: '%s | Nuxt.js tag items viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
