1235bb9b3bb921f4dd45a46c021d95b4390d7804
takamuraTK <tirpitz.zzz@outlook.jp>
2020-08-30 02:56:37 +0900

    Move to app

--- nuxt-blog-service/nuxt.config.js
+++ nuxt-blog-service/nuxt.config.js
@@ -5,6 +5,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-mode
   */
   mode: 'universal',
+  srcDir: 'app',
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
